81e1b51ffcd0f28609b8a9d24d9870f72512c222
Mohammed Khan <m.khan935@gmail.com>
2017-01-17 15:56:24 -0500

    turned off debugging

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3)_!ic8^f+jv6)-!gtc644!n55_ogs#(bo@q2%tt!9q-tj7usw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['adfreeweatherapp.herokuapp.com']
 
